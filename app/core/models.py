c6d58a0dc96e25333346d9ab06e30fa913fb327a
Jonathan Gibbons <jonathan.gibbons6@gmail.com>
2022-07-11 10:33:57 +0100

    Normalised emails on user signup

--- app/core/models.py
+++ app/core/models.py
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
         """Create, save and return a new user."""
         # self.model is accessible since the manager is
         # associated to the User model.
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
