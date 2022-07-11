214283ae74ff6e8ed05bbb0e10cf7e8c3403a904
Jonathan Gibbons <jonathan.gibbons6@gmail.com>
2022-07-11 22:30:47 +0100

    TDD - Defined test case on django admin for  displaying users in database

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,38 @@
+"""
+Tests for the Django admin modifications.
+"""
+import email
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Tests for django admin."""
+
+    def setUp(self):
+        """Create user and client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        # Force the client to use the admin user account for all tests.
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User',
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on page."""
+        # Request predefined changelist page from django admin.
+        url = reverse('admin:core_user_changelist')
+        # Http get request to list of users page.
+        res = self.client.get(url)
+
+        # Assert that the users name and email are displayed on the page.
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+        
